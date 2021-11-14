0986aed385fff9efac5315e6752f3a0eaeaceb63
+ <+>
2021-11-14 15:45:19 -0700

    add methods to format call history date, phone, and duration

--- src/components/CallHistory.js
+++ src/components/CallHistory.js
@@ -1,13 +1,38 @@
 import React from "react";
+import moment from 'moment';
 
 const CallHistory = (props) => {
     
+    const formattedTime = (dateTime) => {
+        return moment(dateTime).format('L LT');
+    }
+
+    const formattedPhone = (phoneNumber) => {
+        let cleaned = ('' + phoneNumber).replace(/\D/g, '');
+        let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+
+        return '(' + match[1] + ') ' + match[2] + '-' + match[3];
+    }
+
+    const formattedDuration = (duration) => {
+        let minutes = Math.floor(duration / 60).toString();
+        let seconds = (duration % 60).toString();
+
+        if (seconds.length === 1) {
+            seconds = ('0' + seconds).slice(-2)
+        }
+
+        return minutes + ':' + seconds
+    }
+
     return (
         <div>
             {props.calls.map(call => {
                 return (
                     <div key={call.created_at}>
-                        {call.created_at} - {call.phone_number} - {call.duration}
+                        &nbsp; {formattedTime(call.created_at)}
+                        &nbsp; {formattedPhone(call.phone_number)}
+                        &nbsp; {formattedDuration(call.duration)}
                     </div>
                 )
             })}
